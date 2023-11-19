1d5aed15dd8556b898e4a2dea6b5d8d4a1bcb1a5
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-11-19 16:13:19 -0300

    chore(setup): first commit with setup files

---
+++ src/main/java/com/javacourse/dynamiclist/DynamiclistApplication.java
@@ -0,0 +1,13 @@
+package com.javacourse.dynamiclist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamiclistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamiclistApplication.class, args);
+	}
+
+}
